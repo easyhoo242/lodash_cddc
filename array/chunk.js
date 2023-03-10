17b9951c07cfb49b58077b216c8c3b13e7e4abad
Du Cheng <easyhoo2426@163.com>
2023-03-10 18:55:48 +0800

    feat: build

---
+++ array/chunk.js
@@ -0,0 +1,7 @@
+/**
+ * @description 将数组（array）拆分成多个 size 长度的区块，并将这些区块组成一个新数组。 如果array 无法被分割成全部等长的区块，那么最后剩余的元素将组成一个区块。
+ * @param {Array} array 要处理的数组
+ * @param {Number} [size=1] 每个数组区块的长度
+ * @returns {Array} 返回一个包含拆分区块的新数组
+ */
+export function chunk(array, size = 1) {}
